212d25889b2ecd74bff93fdcc957c2a678977322
Orlando Mendoza <ommv.17@gmail.com>
2024-03-19 01:21:55 -0400

    Algunos cambios

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ console.log("NODE_ENV ->", NODE_ENV);
 console.log("Development Build ->", dev);
 console.log("PORT ->", port);
 
+const dbHost = process.env.DB_HOST
+const dbUser = process.env.DB_USER
+const dbName = process.env.DB_NAME
+const dbPassword = process.env.DB_PASSWORD
+const dbInstance = process.env.DB_INSTANCE
+
 console.log("dbHost: ", dbHost)
 console.log("dbUser: ", dbUser)
 console.log("dbName: ", dbName)
